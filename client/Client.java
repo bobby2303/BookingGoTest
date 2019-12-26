74a76a1f90745740e6cbec4ea0b867fe636ad8ba
Bobby Rangoonwala <bobbyrangoonwala@outlook.com>
2019-12-26 21:48:38 +0000

    v1 Handling of supplier API errors
    - Handling of timeout error from API
    - Handling of no response from API
    - General error handling
    - No longer using static methods, Client method instantiated in main

--- client/Client.java
+++ client/Client.java
@@ -14,10 +14,9 @@ import java.util.concurrent.TimeUnit ;
 public class Client
 {
     private static final OkHttpClient client = new OkHttpClient().newBuilder().readTimeout(2000, TimeUnit.MILLISECONDS).build();
-
     private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
-    private static JSONObject parseRequest(String jsonString)
+    private JSONObject parseRequest(String jsonString)
     {
         JSONTokener jt = new JSONTokener(jsonString);
         String output = "";
@@ -30,70 +29,77 @@ public class Client
         return json;
     }
 
-    private static JSONObject getRequest(String url) throws IOException
+    private JSONObject getRequest(String url, String supplier)
     {
+        JSONObject json = null;
         //GET Request
         Request request = new Request.Builder().url(url).get().build();
         try (Response response = client.newCall(request).execute())
         {
-            JSONObject json = parseRequest(response.body().string());
+            json = parseRequest(response.body().string());
             if(response.code() != 200)
             {
-                System.out.println(json.get("status") + ": " + json.get("error"));
-                System.out.println(json.get("message"));
-                System.exit(1);
+                System.out.println(supplier + " ERROR " + json.get("status") + ": " + json.get("error") + " - " + json.get("message"));
             }
             System.out.println(json.toString());
             return json;
         }
+        catch(Exception e)
+        {
+            System.out.println("RESPONSE ERROR: No response from " + supplier);
+        }
+        return json;
     }
 
-    private static List<Option> getOptions(JSONObject[] jsons, int maxPassengers)
+    private List<Option> getOptions(List<JSONObject> jsons, int maxPassengers)
     {
         List<Option> options = new ArrayList<Option>();
-        for(JSONObject json: jsons)
+        try
         {
-            //Create Options[] array
-            String supplier = (String) json.get("supplier_id");
-            JSONArray jsonArray = (JSONArray) json.get("options");
-
-            for (int i=0 ; i<jsonArray.length(); i++)
+            for (JSONObject json : jsons)
             {
-                JSONObject obj = jsonArray.getJSONObject(i);
-                String carType = (String) obj.get("car_type");
-                int price = (int) obj.get("price");
+                //Create Options[] array
+                String supplier = (String) json.get("supplier_id");
+                JSONArray jsonArray = (JSONArray) json.get("options");
 
-                if(maxPassengers <= 4 && maxPassengers > 0)
+                for (int i = 0; i < jsonArray.length(); i++)
                 {
-                    if(!(carType.equals("PEOPLE_CARRIER") || carType.equals("LUXURY_PEOPLE_CARRIER") || carType.equals("MINIBUS")))
+                    JSONObject obj = jsonArray.getJSONObject(i);
+                    String carType = (String) obj.get("car_type");
+                    int price = (int) obj.get("price");
+
+                    if (maxPassengers <= 4 && maxPassengers > 0)
+                    {
+                        if (!(carType.equals("PEOPLE_CARRIER") || carType.equals("LUXURY_PEOPLE_CARRIER") || carType.equals("MINIBUS")))
+                        {
+                            Option o = comparePrice(options, supplier, carType, price);
+                            if (!(options.contains(o))) options.add(o);
+                        }
+                    }
+                    else if (maxPassengers <= 6 && maxPassengers > 4)
+                    {
+                        if (!(carType.equals("MINIBUS")))
+                        {
+                            Option o = comparePrice(options, supplier, carType, price);
+                            if (!(options.contains(o))) options.add(o);
+                        }
+                    }
+                    else if (maxPassengers <= 16 && maxPassengers > 6)
                     {
                         Option o = comparePrice(options, supplier, carType, price);
-                        if(!(options.contains(o)))
-                            options.add(o);
+                        if (!(options.contains(o))) options.add(o);
                     }
-
-                }
-                else if(maxPassengers <= 6 && maxPassengers > 4)
-                {
-                    if(!(carType.equals("MINIBUS")))
-                    {
-                        Option o = comparePrice(options, supplier, carType, price);
-                        if(!(options.contains(o)))
-                            options.add(o);
-                    }
-                }
-                else if(maxPassengers <= 16 && maxPassengers > 6)
-                {
-                    Option o = comparePrice(options, supplier, carType, price);
-                    if(!(options.contains(o)))
-                        options.add(o);
                 }
             }
+        }
+        catch(Exception e)
+        {
+            System.out.println("ERROR");
         }
         return options;
     }
 
-    private static Option comparePrice(List<Option> options, String newSupplier, String newCarType, int newPrice)
+    private Option comparePrice(List<Option> options, String newSupplier, String newCarType, int newPrice)
     {
         for(Option o : options)
         {
@@ -110,12 +116,50 @@ public class Client
         }
         Option unchangedOption = new Option(newSupplier, newCarType, newPrice);
         return unchangedOption;
-        //Get the option
-        //Do to String
-        //contains (new Car Type)
-        //comrapre price
-        // return true or false
-        // whether to add this new car type to the list or not
+    }
+
+    public void run(String[] args) throws IOException
+    {
+        try
+        {
+            float pickupLat = Float.parseFloat(args[0]);
+            float pickupLong = Float.parseFloat(args[1]);
+            float dropoffLat = Float.parseFloat(args[2]);
+            float dropoffLong = Float.parseFloat(args[3]);
+            int maxPassengers = Integer.parseInt(args[4]);
+
+            if(maxPassengers > 16 || maxPassengers < 1)
+            {
+                System.out.println("Number of max passengers is not applicable. Must be between 1-16");
+                System.exit(1);
+            }
+
+            String DaveURL = "https://techtest.rideways.com/dave?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
+            String EricURL = "https://techtest.rideways.com/eric?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
+            String JeffURL = "https://techtest.rideways.com/jeff?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
+            //call
+            List<JSONObject> jsons = new ArrayList<JSONObject>();
+            JSONObject daveJSON = getRequest(DaveURL, "Dave");
+            if(daveJSON != null) jsons.add(daveJSON);
+            JSONObject ericJSON = getRequest(EricURL, "Eric");
+            if(ericJSON != null) jsons.add(ericJSON);
+            JSONObject jeffJSON = getRequest(JeffURL, "Jeff");
+            if(jeffJSON != null) jsons.add(jeffJSON);
+
+            List<Option> options = getOptions(jsons, maxPassengers);
+
+            Collections.sort(options);
+
+            for(Option o: options)
+            {
+                System.out.printf("%-21s - %-4s - %-8d \n", o.getCarType(), o.getSupplier(), o.getPrice());
+            }
+
+        }
+        catch(NumberFormatException nfe)
+        {
+            System.out.println("Arguments must all be numbers");
+        }
     }
 
 
@@ -123,48 +167,8 @@ public class Client
     {
         if(args.length > 0)
         {
-            try
-            {
-                float pickupLat = Float.parseFloat(args[0]);
-                float pickupLong = Float.parseFloat(args[1]);
-                float dropoffLat = Float.parseFloat(args[2]);
-                float dropoffLong = Float.parseFloat(args[3]);
-                int maxPassengers = Integer.parseInt(args[4]);
-
-                if(maxPassengers > 16 || maxPassengers < 1)
-                {
-                    System.out.println("Number of max passengers is not applicable. Must be between 1-16");
-                    System.exit(1);
-                }
-
-
-                String DaveURL = "https://techtest.rideways.com/dave?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
-                String EricURL = "https://techtest.rideways.com/eric?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
-                String JeffURL = "https://techtest.rideways.com/jeff?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
-                //call
-                JSONObject[] jsons = new JSONObject[3];
-                jsons[0] = getRequest(DaveURL);
-                jsons[1] = getRequest(EricURL);
-                jsons[2] = getRequest(JeffURL);
-
-                List<Option> options = getOptions(jsons, maxPassengers);
-
-
-
-
-                Collections.sort(options);
-
-                for(Option o: options)
-                {
-                    System.out.printf("%-21s - %-4s - %-8d \n", o.getCarType(), o.getSupplier(), o.getPrice());
-                }
-
-
-            }
-            catch(NumberFormatException nfe)
-            {
-                System.out.println("Arguments must all be numbers");
-            }
+            Client c = new Client();
+            c.run(args);
         }
         else
         {
