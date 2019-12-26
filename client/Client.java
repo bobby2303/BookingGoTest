802263d156af7002e0f580f53739da39f4e60f47
Bobby Rangoonwala <bobbyrangoonwala@outlook.com>
2019-12-26 16:14:02 +0000

    v0.5 Eric and Jeff API
    - able to call multiple different APIs

--- client/Client.java
+++ client/Client.java
@@ -9,10 +9,12 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONTokener;
 import java.util.*;
+import java.util.concurrent.TimeUnit ;
 
 public class Client
 {
-    private static OkHttpClient client = new OkHttpClient();
+    private static final OkHttpClient client = new OkHttpClient().newBuilder().readTimeout(10000, TimeUnit.MILLISECONDS).build();
+
     private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     private static JSONObject parseRequest(String jsonString)
@@ -26,6 +28,58 @@ public class Client
         }
         JSONObject json = new JSONObject(output);
         return json;
+    }
+
+    private static JSONObject getRequest(String url) throws IOException
+    {
+        //GET Request
+        Request request = new Request.Builder().url(url).get().build();
+        try (Response response = client.newCall(request).execute())
+        {
+            JSONObject json = parseRequest(response.body().string());
+            if(response.code() != 200)
+            {
+                System.out.println(json.get("status") + ": " + json.get("error"));
+                System.out.println(json.get("message"));
+                System.exit(1);
+            }
+            System.out.println(json.toString());
+            return json;
+        }
+    }
+
+    public static List<Option> comparePrices(JSONObject[] jsons, int maxPassengers)
+    {
+        List<Option> options = new ArrayList<Option>();
+        for(JSONObject json: jsons)
+        {
+            //Create Options[] array
+            String supplier = (String) json.get("supplier_id");
+            JSONArray jsonArray = (JSONArray) json.get("options");
+
+            for (int i=0 ; i<jsonArray.length(); i++)
+            {
+                JSONObject obj = jsonArray.getJSONObject(i);
+                String carType = (String) obj.get("car_type");
+                int price = (int) obj.get("price");
+
+                if(maxPassengers <= 4 && maxPassengers > 0)
+                {
+                    if(!(carType.equals("PEOPLE_CARRIER") || carType.equals("LUXURY_PEOPLE_CARRIER") ||carType.equals("MINIBUS")))
+                        options.add(new Option(supplier, carType, price));
+                }
+                else if(maxPassengers <= 6 && maxPassengers > 4)
+                {
+                    if(!(carType.equals("MINIBUS")))
+                        options.add(new Option(supplier, carType, price));
+                }
+                else if(maxPassengers <= 16 && maxPassengers > 6)
+                {
+                    options.add(new Option(supplier, carType, price));
+                }
+            }
+        }
+        return options;
     }
 
     public static void main (String[] args) throws IOException
@@ -46,50 +100,20 @@ public class Client
                     System.exit(1);
                 }
 
-                JSONObject json;
-                String url = "https://techtest.rideways.com/dave?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
 
-                //GET Request
-                Request request = new Request.Builder().url(url).get().build();
-                try (Response response = client.newCall(request).execute())
-                {
-                    json = parseRequest(response.body().string());
-                    if(response.code() != 200)
-                    {
-                        System.out.println(json.get("status") + ": " + json.get("error"));
-                        System.out.println(json.get("message"));
-                        System.exit(1);
-                    }
+                String DaveURL = "https://techtest.rideways.com/dave?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
+                String EricURL = "https://techtest.rideways.com/eric?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
+                String JeffURL = "https://techtest.rideways.com/jeff?pickup=" + pickupLat + "," + pickupLong + "&dropoff=" + dropoffLat + "," + dropoffLong;
+                //call
+                JSONObject[] jsons = new JSONObject[3];
+                jsons[0] = getRequest(DaveURL);
+                jsons[1] = getRequest(EricURL);
+                jsons[2] = getRequest(JeffURL);
+
+                List<Option> options = comparePrices(jsons, maxPassengers);
 
 
-                }
 
-                //System.out.println(json.toString());
-
-                //Create Options[] array
-                JSONArray jsonArray = (JSONArray) json.get("options");
-                List<Option> options = new ArrayList<Option>();
-                for (int i = 0 ; i < jsonArray.length(); i++)
-                {
-                    JSONObject obj = jsonArray.getJSONObject(i);
-                    String carType = (String) obj.get("car_type");
-
-                    if(maxPassengers <= 4 && maxPassengers > 0)
-                    {
-                        if(!(carType.equals("PEOPLE_CARRIER") || carType.equals("LUXURY_PEOPLE_CARRIER") ||carType.equals("MINIBUS")))
-                            options.add(new Option("DAVE", (String) obj.get("car_type"), (int) obj.get("price")));
-                    }
-                    else if(maxPassengers <= 6 && maxPassengers > 4)
-                    {
-                        if(!(carType.equals("MINIBUS")))
-                            options.add(new Option("DAVE", (String) obj.get("car_type"), (int) obj.get("price")));
-                    }
-                    else if(maxPassengers <= 16 && maxPassengers > 6)
-                    {
-                        options.add(new Option("DAVE", (String) obj.get("car_type"), (int) obj.get("price")));
-                    }
-
-                }
 
                 Collections.sort(options);
 
@@ -113,7 +137,8 @@ public class Client
     }
 }
 
-
+//supplier_id in JSONObject
+//{"dropoff":"58.16724,-0.53187","options":[{"price":393381,"car_type":"STANDARD"},{"price":684384,"car_type":"EXECUTIVE"},{"price":142471,"car_type":"LUXURY"},{"price":562193,"car_type":"PEOPLE_CARRIER"},{"price":60544,"car_type":"MINIBUS"}],"pickup":"51.47002,-0.454295","supplier_id":"DAVE"}
 //https://techtest.rideways.com/dave?dropoff=51.470020,-0.454295&pickup=52.167241,-0.443187
 //java -cp .:json-20190722.jar:okhttp-4.2.2.jar:okio-2.0.0.jar:kotlin-stdlib-common-1.3.50.jar:kotlin-stdlib-1.3.50.jar Client 51.470020 -0.454295 58.167241 -0.53187
 //java -cp .:json-20190722.jar:okhttp-4.2.2.jar:okio-2.0.0.jar:kotlin-stdlib-common-1.3.50.jar:kotlin-stdlib-1.3.50.jar Client 51.470020 -0.454295 52.167241 -0.443187
